577a79d7110cda922385e4db11eff8ac1e993805
sisidjidji <sahr1560@gmail.com>
2020-01-14 20:07:40 -0600

    d

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var htmldemo = prompt('DO YOU HAVE ANY HTML KNOWLEDGE yes or not');
+
+var message;
+
+    if( htmldemo === 'yes' || htmldemo ==='YES'){
+
+       message = 'welcome this web page is for you';
+    }
+
+    else {
+
+        message = 'welcome!';
+    }
+
+    document.write(message);
+
+    
